9bd0864da05a132d35c7e43cb2ab612bd2d12e4b
timurmoore <63285672+timurmoore@users.noreply.github.com>
2020-08-11 00:43:52 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -7,20 +7,18 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = "https://telegra.ph/file/1cfb6d07372fd5876e3ef.jpg"
-pm_caption = "`MOORE IS:` **ONLINE**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**MOORE OS** : `3.14`\n"
-pm_caption += "**Current Sat** : `StarkGangSat-2.25`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption = " MOORE HELPER:` **ONLINE**\n\n"
+pm_caption += "**СИСТЕМНАЯ ИНФОРМАЦИЯ**\n"
+pm_caption += "`ВЕРСИЯ TELETHON:` **6.0.9**\n`Python:` **3.7.4**\n"
+pm_caption += "`БАЗА ДАННЫХ:` **В норме**\n"
+pm_caption += "**Текущий бранч** : `master`\n"
+pm_caption += "**Версия MOORE HELPER** : `3.14`\n"
+pm_caption += f"**Мой Boss** : {DEFAULTUSER} \n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     chat = await alive.get_chat()
     await alive.delete()
-    """ For .alive command, check if the bot is running.  """
+    """ Для использования команды .alive бот должен быть запущен.  """
     await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
     await alive.delete() 
