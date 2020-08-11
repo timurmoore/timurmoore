9f900e478f002a7194912e01123bbf2b9a1a1241
timurmoore <63285672+timurmoore@users.noreply.github.com>
2020-08-11 15:50:12 +0300

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -84,7 +84,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             reply_pop_up_alert = "Не тыкай, у тебя нет прав!"
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 8
+    number_of_rows = 5
     number_of_cols = 2
     helpable_plugins = []
     for p in loaded_plugins:
@@ -92,7 +92,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format("👁", x, "👁"),
+        "{} {} {}".format("☾", x, "☾"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
